15070a402ff70642a5f9ce51c0eb65519fb2bb5e
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2025-03-02 01:30:11 +0000

    fix(AI-CAHT): AI-CAHT

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function Home() {
+export default function page() {
   return (
     <div>
       <Link href={"/dashboard"}>go to dashboard</Link>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Link from "next/link"
+import {SignedIn, UserButton} from "@clerk/nextjs"
+
+export default function Navbar() {
+
+  return (
+    <SignedIn>
+      <header>
+        <nav className={"flex justify-between items-center px-5 py-3 bg-[#212121]"}>
+          <Link href={"/home"} className={"flex gap-2.5 items-center"}>
+            <img src={"/logo.png"} alt={"image logo"} className={"w-10 h-10"} />
+            <span>LAMA AI</span>
+          </Link>
+          <UserButton />
+        </nav>
+      </header>
+    </SignedIn>
+  )
+}
